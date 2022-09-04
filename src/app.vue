1a580009a6d292f6ffff94b9a4c5445f850e627b
wins <win4709hi@gmail.com>
2022-09-04 18:37:37 +0800

    1. 取代vue-cli，手動搭建一個vue專案。基本webpack設定

---
+++ src/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="count++">
+    點我增加
+  </button>
+  <div>
+    {{ count }}
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'App',
+  setup() {
+    const count = ref(0);
+    
+    return {
+      count
+    }
+  }
+}
+</script>
